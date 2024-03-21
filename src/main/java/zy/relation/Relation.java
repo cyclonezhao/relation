ab9786b1ee53de79f64c410a0daf9a10b882f625
zhaoyi <lightseeker@qq.com>
2024-03-21 17:17:37 +0800

    改为依赖H2数据库，添加spring-boot-starter-web依赖

---
+++ src/main/java/zy/relation/Relation.java
@@ -0,0 +1,11 @@
+package zy.relation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Relation {
+    public static void main(String[] args) {
+        SpringApplication.run(Relation.class, args);
+    }
+}
